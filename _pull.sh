f55d11d30fee30b5bf72ddad6aa6bc41dd165b8a
liaoyj <liaoyj@gitissoft.com>
2022-06-10 17:01:45 +0800

    -

---
+++ _pull.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo ---pull from gitee dev---
+git pull origin master
+read -p "Press any key to continue." 
---
+++ _push.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo ---push to gitee dev---
+git push origin master
+read -p "Press any key to continue."
